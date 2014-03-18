4b000640133e163499a792ad325dbc169af75414
Jacob Sparre Andersen <jsa@adaheads.com>
2014-03-18 10:19:02 +0000

    Demo web server. - Just to make sure everything is compiled.

--- src/demo.adb
+++ src/demo.adb
@@ -1,4 +1,33 @@
+with
+  Ada.Text_IO;
+with
+  Black.HTTP.Request,
+  Black.HTTP.Server;
+--  GNAT.Sockets.Compatibility;
+
+--  pragma Unreferenced (GNAT.Sockets.Compatibility); --  Just to build it.
+
 procedure Demo is
+   use Black;
 begin
-   null;
+   Ada.Text_IO.Put_Line ("Demo started");
+
+   declare
+      procedure Redirect_To_Jacob (Request : in out HTTP.Request.Instance);
+      procedure Redirect_To_Jacob (Request : in out HTTP.Request.Instance) is
+         use HTTP.Request;
+      begin
+         Redirect (Request   => Request,
+                   Target    => "http://www.jacob-sparre.dk/",
+                   Permanent => False);
+      end Redirect_To_Jacob;
+
+      package Server is
+         new Black.HTTP.Server (Handler => Redirect_To_Jacob);
+   begin
+      Server.Start (Listen_To => 8042);
+      Ada.Text_IO.Put_Line ("HTTP server created");
+      delay 86_000.0;
+      Server.Stop;
+   end;
 end Demo;
