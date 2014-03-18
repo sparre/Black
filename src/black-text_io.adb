3d012a92e08e4c6ee6f906b00b8b841b5caa8aed
Jacob Sparre Andersen <jsa@adaheads.com>
2014-03-18 12:34:28 +0000

    Stream based Get_Line operations.

--- src/black-text_io.adb
+++ src/black-text_io.adb
@@ -1,7 +1,37 @@
 with
-  Ada.Characters.Latin_1,
-  Ada.Streams;
+  Ada.Characters.Latin_1;
+
 package body Black.Text_IO is
+   function Get_Line
+     (Stream : not null access Ada.Streams.Root_Stream_Type'Class)
+     return String is
+   begin
+      return Ada.Strings.Unbounded.To_String (Get_Line (Stream));
+   end Get_Line;
+
+   function Get_Line
+     (Stream : not null access Ada.Streams.Root_Stream_Type'Class)
+     return Ada.Strings.Unbounded.Unbounded_String is
+      use Ada.Characters.Latin_1, Ada.Strings.Unbounded;
+      Buffer : Unbounded_String := Null_Unbounded_String;
+      Next   : Character;
+   begin
+      loop
+         Character'Read (Stream, Next);
+
+         if Next = CR then
+            Character'Read (Stream, Next);
+            if Next = LF then
+               return Buffer;
+            else
+               Append (Buffer, CR & Next);
+            end if;
+         else
+            Append (Buffer, Next);
+         end if;
+      end loop;
+   end Get_Line;
+
    procedure New_Line (File : in     POSIX.IO.File_Descriptor) is
    begin
       Put (File => File,
--- src/black-text_io.ads
+++ src/black-text_io.ads
@@ -1,5 +1,9 @@
 with
+  Ada.Streams,
+  Ada.Strings.Unbounded;
+with
   POSIX.IO;
+
 private
 package Black.Text_IO is
    procedure Put (File : in     POSIX.IO.File_Descriptor;
@@ -7,4 +11,11 @@ package Black.Text_IO is
    procedure Put_Line (File : in     POSIX.IO.File_Descriptor;
                        Item : in     String);
    procedure New_Line (File : in     POSIX.IO.File_Descriptor);
+
+   function Get_Line
+     (Stream : not null access Ada.Streams.Root_Stream_Type'Class)
+     return String;
+   function Get_Line
+     (Stream : not null access Ada.Streams.Root_Stream_Type'Class)
+     return Ada.Strings.Unbounded.Unbounded_String;
 end Black.Text_IO;
