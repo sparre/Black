2c278ab9d51e40a9d3c48341c47304d5ce901e21
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-04-01 14:42:20 +0200

    HTTP string constants and OPTIONS.

--- src/black-http.ads
+++ src/black-http.ads
@@ -1,5 +1,20 @@
 package Black.HTTP is
    pragma Pure;
 
-   type Methods is (Put, Get, Delete, Post);
+   type Methods is (PUT, GET, DELETE, POST, OPTIONS);
+
+private
+
+   HTTP_Part    : constant String := "HTTP/1.1";
+
+   OK           : constant String := HTTP_Part & " 200 OK";
+   No_Content   : constant String := HTTP_Part & " 204 No Content";
+
+   Bad_Request  : constant String := HTTP_Part & " 400 Bad Request";
+   Unauthorized : constant String := HTTP_Part & " 401 Not Authorized";
+   Forbidden    : constant String := HTTP_Part & " 403 Forbidden";
+   Not_Found    : constant String := HTTP_Part & " 404 Not Found";
+
+   Server_Error : constant String := HTTP_Part & " 500 Internal Server Error";
+
 end Black.HTTP;
