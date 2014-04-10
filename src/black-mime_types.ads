e043177a1ca30690d43f638e028a8f6e2fbd9f22
Jacob Sparre Andersen <jsa@adaheads.com>
2014-04-10 11:33:31 +0200

    Introducing a package of some known (and relevant) MIME types.

---
+++ src/black-mime_types.ads
@@ -0,0 +1,9 @@
+package Black.MIME_Types is
+   package Application is
+      JSON : constant String := "application/json";
+   end Application;
+
+   package Text is
+      Plain : constant String := "text/plain; charset=iso-8859-1";
+   end Text;
+end Black.MIME_Types;
